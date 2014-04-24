7eb574ef1d8047e66e08e88cb93cf2328914bc9a
CMS Roma DAQ user <cmsroma.daq@gmail.com>
2014-04-24 17:03:06 +0200

    added Fermi-Dirac+Gauss as an option (global bool)

--- precalib_analysis/bgoCalibration.cpp
+++ precalib_analysis/bgoCalibration.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <cmath>
 
 #include "TH1D.h"
 #include "TFile.h"
@@ -8,6 +9,22 @@
 #include "TF1.h"
 #include "TCanvas.h"
 
+
+
+bool useFermiDirac_ = true;
+
+
+Double_t FermiDirac(Double_t *x, Double_t *par)
+ // Fermi-Dirac distribution
+{
+  return 1./(exp((x[0]-par[0])/par[1])+1.);
+}
+
+
+Double_t FermiDiracPlusGaus(Double_t *x, Double_t *par)
+{
+  return par[0]/(exp((x[0]-par[1])/par[2])+1.) + par[3]*exp(-0.5*((x[0]-par[4])/par[5])*((x[0]-par[4])/par[5])) ;
+}
 
 
 float getSinglePeakPosition( const std::string& outdir, int hv, const std::string& suffix, int iChannel);
@@ -30,7 +47,9 @@ int main() {
   int hv = 1700;
   std::string suffix = "front";
 
-  std::string outdir = Form("bgoCalibration_hv%d_%s", hv, suffix.c_str() );
+  std::string suffix_dir(suffix);
+  if( useFermiDirac_ ) suffix_dir += "_FD";
+  std::string outdir = Form("bgoCalibration_hv%d_%s", hv, suffix_dir.c_str() );
   std::string mkdir_command = "mkdir -p " + outdir;
   system(mkdir_command.c_str());
 
@@ -71,9 +90,11 @@ float getSinglePeakPosition( const std::string& outdir, int hv, const std::strin
 
   TTree* tree = (TTree*)file->Get("eventRawData");
 
+  float adcMax = 1000.;
+
   std::string name = Form("Channel %d", iChannel);
-  TH1D* h1_spectrum_full  = new TH1D(name.c_str(),  "", 1000, 0., 1000.);
-  TH1D* h1_spectrum_trunc = new TH1D("spectrum_trunc", "", 70, 300., 1000.);
+  TH1D* h1_spectrum_full  = new TH1D(name.c_str(),  "", 1000, 0., adcMax );
+  TH1D* h1_spectrum_trunc = new TH1D("spectrum_trunc", "", 70, 300., adcMax );
 
   h1_spectrum_full->SetXTitle( "ADC Counts" );
   
@@ -88,24 +109,134 @@ float getSinglePeakPosition( const std::string& outdir, int hv, const std::strin
   float nSigma_lo = 1.;
   float nSigma_hi = 2.;
 
-  float xMin = mean-nSigma_lo*rms;
-  float xMax = mean+nSigma_hi*rms;
+  float fitMin;
+  float fitMax;
 
-  TF1* f1_gaus = new TF1("gaus0", "gaus", xMin, xMax);
-  f1_gaus->SetParameter(0, integral);
-  f1_gaus->SetParameter(1, mean);
-  f1_gaus->SetParameter(2, rms);
+  TF1* f1_fit;
 
-  f1_gaus->SetLineColor(kRed);
+  if( useFermiDirac_ ) {
 
-  int niter = 4;
-  for( unsigned iter=0; iter<niter; ++iter ) {
-    std::string option = (iter==niter-1) ? "R" : "RNQ";
-    h1_spectrum_full->Fit( f1_gaus, option.c_str() );
-    float mean_new = f1_gaus->GetParameter(1);
-    float rms_new = f1_gaus->GetParameter(2);
-    f1_gaus->SetRange( mean_new-nSigma_lo*rms_new, mean_new+nSigma_hi*rms_new );
+    fitMin = 220.;
+    fitMax = adcMax;
+    if     ( iChannel==1 )  fitMin = 220.;
+    else if( iChannel==2 )  fitMin = 220.;
+    else if( iChannel==3 )  fitMin = 230.;
+    else if( iChannel==4 )  fitMin = 260.;
+    else if( iChannel==5 )  fitMin = 220.;
+    else if( iChannel==6 )  fitMin = 230.;
+    else if( iChannel==7 )  fitMin = 250.;
+    else if( iChannel==8 )  fitMin = 240.;
+
+    f1_fit = new TF1("totFunc", FermiDiracPlusGaus, fitMin, fitMax, 6 );
+
+    // FD normalization:
+    f1_fit->SetParameter(0,300.);
+    //f1_fit->SetParLimits(0,150., 500.);
+    
+    // 1  p0           3.05564e+02   2.02221e+01   3.31646e-02  -2.73120e-04
+    // 2  p1           2.87305e+02   1.05400e+01   1.45260e-02  -5.31457e-04
+    // 3  p2           8.16511e+01   1.60388e+00   8.57439e-03  -3.75531e-04
+    // 4  p3           2.91747e+02   2.47999e+00   2.88441e-02  -3.04096e-04
+    // 5  p4           4.50352e+02   3.33186e-01   4.18731e-03  -2.80640e-03
+    // 6  p5           3.75147e+01   2.99857e-01   2.80790e-03  -3.28132e-03
+
+    // FD flex point:
+    f1_fit->SetParameter(1,400.);
+    //f1_fit->SetParLimits(1,250., 600.);
+    
+    // FD temp:
+    f1_fit->SetParameter(2,80.);
+    //f1_fit->SetParLimits(2,0., 150.);
+    
+    // gauss normalization:
+    f1_fit->SetParameter(3,300.);
+    //f1_fit->SetParLimits(3,0.2*integral, 2.*integral);
+    
+    // gauss mean:
+    //f1_fit->SetParameter(4,450.);
+         if( iChannel==1 ) f1_fit->SetParameter(4, 145.8+192.4);
+    else if( iChannel==2 ) f1_fit->SetParameter(4, 145.8+301.3);
+    else if( iChannel==3 ) f1_fit->SetParameter(4, 145.8+258.5);
+    else if( iChannel==4 ) f1_fit->SetParameter(4, 145.8+290.3);
+    else if( iChannel==5 ) f1_fit->SetParameter(4, 145.8+197.8);
+    else if( iChannel==6 ) f1_fit->SetParameter(4, 145.8+190.8);
+    else if( iChannel==7 ) f1_fit->SetParameter(4, 145.8+225.7);
+    else if( iChannel==8 ) f1_fit->SetParameter(4, 145.8+235.2);
+    //f1_fit->SetParLimits(4,300., 500.);
+    
+    // gauss sigma:
+    f1_fit->SetParameter(5,40.);
+    //f1_fit->SetParLimits(5,10., 100.);
+    
+    f1_fit->SetLineColor(kRed);
+
+    h1_spectrum_full->Fit( f1_fit, "R" );
+    TF1* f1_fit = new TF1("totFunc", FermiDiracPlusGaus, fitMin, adcMax, 6 );
+
+    // FD normalization:
+    f1_fit->SetParameter(0,300.);
+    //f1_fit->SetParLimits(0,150., 500.);
+    
+    // 1  p0           3.05564e+02   2.02221e+01   3.31646e-02  -2.73120e-04
+    // 2  p1           2.87305e+02   1.05400e+01   1.45260e-02  -5.31457e-04
+    // 3  p2           8.16511e+01   1.60388e+00   8.57439e-03  -3.75531e-04
+    // 4  p3           2.91747e+02   2.47999e+00   2.88441e-02  -3.04096e-04
+    // 5  p4           4.50352e+02   3.33186e-01   4.18731e-03  -2.80640e-03
+    // 6  p5           3.75147e+01   2.99857e-01   2.80790e-03  -3.28132e-03
+
+    // FD flex point:
+    f1_fit->SetParameter(1,400.);
+    //f1_fit->SetParLimits(1,250., 600.);
+    
+    // FD temp:
+    f1_fit->SetParameter(2,80.);
+    //f1_fit->SetParLimits(2,0., 150.);
+    
+    // gauss normalization:
+    f1_fit->SetParameter(3,300.);
+    //f1_fit->SetParLimits(3,0.2*integral, 2.*integral);
+    
+    // gauss mean:
+    //f1_fit->SetParameter(4,450.);
+         if( iChannel==1 ) f1_fit->SetParameter(4, 145.8+192.4);
+    else if( iChannel==2 ) f1_fit->SetParameter(4, 145.8+301.3);
+    else if( iChannel==3 ) f1_fit->SetParameter(4, 145.8+258.5);
+    else if( iChannel==4 ) f1_fit->SetParameter(4, 145.8+290.3);
+    else if( iChannel==5 ) f1_fit->SetParameter(4, 145.8+197.8);
+    else if( iChannel==6 ) f1_fit->SetParameter(4, 145.8+190.8);
+    else if( iChannel==7 ) f1_fit->SetParameter(4, 145.8+225.7);
+    else if( iChannel==8 ) f1_fit->SetParameter(4, 145.8+235.2);
+    //f1_fit->SetParLimits(4,300., 500.);
+    
+    // gauss sigma:
+    f1_fit->SetParameter(5,40.);
+
+  } else { // iterative gaussian fit
+
+    fitMin = mean-nSigma_lo*rms;
+    fitMax = mean+nSigma_hi*rms;
+
+    TF1* f1_gaus = new TF1("gaus0", "gaus", fitMin, fitMax);
+    f1_gaus->SetParameter(0, integral);
+    f1_gaus->SetParameter(1, mean);
+    f1_gaus->SetParameter(2, rms);
+
+    int niter = 4;
+    for( unsigned iter=0; iter<niter; ++iter ) {
+      std::string option = (iter==niter-1) ? "R" : "RNQ";
+      h1_spectrum_full->Fit( f1_gaus, option.c_str() );
+      float mean_new = f1_gaus->GetParameter(1);
+      float rms_new = f1_gaus->GetParameter(2);
+      f1_gaus->SetRange( mean_new-nSigma_lo*rms_new, mean_new+nSigma_hi*rms_new );
+    }
+
   }
+
+  
+  f1_fit->SetLineColor(kRed);
+
+  h1_spectrum_full->Fit( f1_fit, "R" );
+
 
 
 
@@ -120,7 +251,8 @@ float getSinglePeakPosition( const std::string& outdir, int hv, const std::strin
   delete c1;
 
 
-  return f1_gaus->GetParameter(1);
+  return f1_fit->GetParameter(4);
+  //return f1_gaus->GetParameter(1);
 
 }
   
